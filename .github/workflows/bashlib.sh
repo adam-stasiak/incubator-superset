510f208168bcf6317d8eb7a53339a1849dec7917
Adam Stasiak <adam.stasiak@polidea.com>
2020-09-15 16:32:08 +0200

    fixed path and set one test

--- .github/workflows/bashlib.sh
+++ .github/workflows/bashlib.sh
@@ -185,7 +185,7 @@ cypress-run-all() {
   nohup flask run --no-debugger -p $port >"$flasklog" 2>&1 </dev/null &
   local flaskProcessId=$!
 
-  cypress-run "*/**/*"
+  cypress-run "*/**/card_view.test.ts"
 
   # Upload code coverage separately so each page can have separate flags
   # -c will clean existing coverage reports, -F means add flags
