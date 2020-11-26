5f2aa0bea77e86d19064626cb3c8f213c792eb95
Adam Stasiak <adam.stasiak@polidea.com>
2020-11-26 09:42:16 +0100

    Cypress docker

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -38,19 +38,25 @@ Init Step ${1}/${STEP_CNT} [${2}] -- ${3}
 EOF
 }
 
+ADMIN_PASSWORD="admin"
+# If Cypress run which consumes superset_test_config – overwrite the password for admin
+if [ "$SUPERSET_CONFIG" = "tests.superset_test_config" ]; then
+    ADMIN_PASSWORD="general"
+fi
+
 # Initialize the database
 echo_step "1" "Starting" "Applying DB migrations"
 superset db upgrade
 echo_step "1" "Complete" "Applying DB migrations"
 
 # Create an admin user
-echo_step "2" "Starting" "Setting up admin user ( admin / admin )"
+echo_step "2" "Starting" "Setting up admin user ( admin / $ADMIN_PASSWORD )"
 superset fab create-admin \
               --username admin \
               --firstname Superset \
               --lastname Admin \
               --email admin@superset.com \
-              --password admin
+              --password $ADMIN_PASSWORD
 echo_step "2" "Complete" "Setting up admin user"
 
 # Create default roles and permissions
@@ -61,6 +67,12 @@ echo_step "3" "Complete" "Setting up roles and perms"
 if [ "$SUPERSET_LOAD_EXAMPLES" = "yes" ]; then
     # Load some data to play with
     echo_step "4" "Starting" "Loading examples"
-    superset load_examples
+    # If Cypress run which consumes superset_test_config – load required data for tests
+    if [ "$SUPERSET_CONFIG" = "tests.superset_test_config" ]; then
+        superset load_test_users
+        superset load_examples --load-test-data
+    else
+        superset load_examples
+    fi
     echo_step "4" "Complete" "Loading examples"
 fi
